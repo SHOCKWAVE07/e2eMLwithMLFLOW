e510a23369364a19e2c09fafcea388b3f5a0409d
shockwave07 <varunraskar22@gmail.com>
2023-08-04 01:23:10 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.e2emlProject import logger
 from e2emlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
 from e2emlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from e2emlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,17 @@ try:
     obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>> {STAGE_NAME} completed <<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>{STAGE_NAME} completed <<<<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
